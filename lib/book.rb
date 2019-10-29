e1dda957cc749c6bdec8d6e08a51e108a740420a
Griffin Poole <griffinpoole5@gmail.com>
2019-10-29 15:31:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,10 @@
 class Book
   
-  attr_reader :title
+  attr_reader :title, :author_name
   
-  def initialize(title)
+  def initialize(title, name)
     @title = title
+    @author_name = name
   end
   
 end
